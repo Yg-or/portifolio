b3bf16b4c0a768f67a665d5c32e9371f8f4e41f4
ygor <inygu@icloud.com>
2021-03-12 14:51:05 -0300

    primeiro teste

--- pages/index.js
+++ pages/index.js
@@ -3,58 +3,52 @@ import React from 'react';
 const mystyle = {
     all:{
     color: "white",
-    backgroundColor: "DodgerBlue",
     padding: "10px",
     fontFamily: "Arial",
-    height: 250,
-    width: 250,
-    backgroundImage: "https://yt3.ggpht.com/ytc/AAUvwnh3qcXoKDGfaEE_tqvBu-VCHcUg0lEPXjBNFT4rgA=s88-c-k-c0x00ffffff-no-rj"
-
-    },
-    teste:{
-    color: "white",
-    backgroundColor: "#f00",
-    padding: "10px",
-    fontFamily: "Arial",
-    width: 300,
-    borderRadius: 30,
-    marginTop: 20
+    height: 150,
+    width: 150,
+    marginTop: 30
 
     },
     tudo:{
-        height: '100%',
-        backgroundColor: '#dadada',
+        backgroundColor: '#FFF',
         display: 'flex',
         justifyContent: 'center',
         flexDirection: 'column',
         alignItems: 'center',
         margin: 0,
-        padding: 0
+        padding: 0,
+        height: '100%'
     },
-    btn:{
-        border: 1,
-        outline: 1,
+    novo:{
+        backgroundColor: "#4286f4",
         color: "white",
-        backgroundColor: "#f00",
-        padding: "10px",
-        fontFamily: "Arial",
-        width: 300,
-        borderRadius: 30,
-        marginTop: 20,
-        height: 100
-    },
-    but:{
-        width:'40%',
-        margin:0.7,
+        fontSize: 20,
+        width: 320,
         height: 40,
-        lineHeight:2.8,
-        borderRadius:7,
-        backgroundColor:'#0070f3',
-        boxShadow:'0 4 14 0',
-        color:"white",
+        borderRadius: 20,
+        margin: "10px 0px",
+        cursor: "pointer",
+        
     },
     img:{
-        width: 250
+        height: '100%',
+        width: '100%',
+        borderRadius: 200,
+        borderStyle: 'solid',
+        borderColor: '#000'
+    },
+    nome:{
+        fontSize: 25,
+        fontFamily: 'sans-serif'
+    },
+    volta:{
+        width: 400,
+        backgroundColor: '#dadada',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
     }
     
   };
@@ -63,11 +57,21 @@ function Home(){
     return(
         <div style={mystyle.tudo}>
             <div style={mystyle.all}>
+                <img src="https://media-exp1.licdn.com/dms/image/C5603AQF5GgUfdmTtkw/profile-displayphoto-shrink_400_400/0/1601997833426?e=1620864000&v=beta&t=frT0oQvT2uvGptF39yM4wGyJdWi1FaAE98-3Uucq0HY" alt="Minha Figura" style={mystyle.img}/>
                 
             </div>
-            <div style={mystyle.teste}>Home 2</div>
-            <button style={mystyle.btn}  href="https://www.google.com" block> teste </button>
-            <button href="#" style={mystyle.but}>Link</button>
+            <h1 style={mystyle.nome}>Ygor Lage</h1>
+            <a href="https://www.instagram.com/inygu/" target="_blank"><button style={mystyle.novo}>Instagram</button></a>
+            <a href="https://twitter.com/itz_ygu" target="_blank"><button style={mystyle.novo}>Twitter</button></a>
+            <a href="https://www.linkedin.com/in/ygor-lage-da-costa-silva-a5183b1ab/" target="_blank"><button style={mystyle.novo}>Linkedin</button></a>
+            <a href="https://api.whatsapp.com/send?phone=5522988011635" target="_blank"><button style={mystyle.novo}>Whatsapp</button></a>
+
+
+
+
+
+
+            
 
         </div>
         
